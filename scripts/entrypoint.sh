210fd4e30265cbb8402938b009a34fcc267c43f0
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2026-04-27 15:59:48 +0300

    Update entrypoint.sh
    Signed-off-by: IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-# Sleep for 2 seconds to ensure container is ready
-sleep 2
 
-cd /home/container
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 
 # Make internal Docker IP address available to processes.
@@ -11,7 +8,7 @@ export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 # Check if already installed
 if [ ! -e "$HOME/.installed" ]; then
     $HOME/usr/local/bin/proot \
-    --rootfs="/" \
+    --rootfs="${HOME}" \
     -0 -w "/root" \
     -b /dev -b /sys -b /proc \
     --kill-on-exit \
