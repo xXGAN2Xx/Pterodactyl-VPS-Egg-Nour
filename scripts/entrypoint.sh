7b54b90421a6162a3b072d17088eb5d2f617b68e
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2026-04-27 14:36:57 +0300

    Update entrypoint.sh
    Signed-off-by: IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,13 +10,13 @@ export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 
 # Check if already installed
 if [ ! -e "$HOME/.installed" ]; then
-    /usr/local/bin/proot \
-    --rootfs="/" \
-    -0 -w "/root" \
+    $HOME/usr/local/bin/proot \
+    --rootfs="$HOME/" \
+    -0 -w "$HOME/root" \
     -b /dev -b /sys -b /proc \
     --kill-on-exit \
-    /bin/sh "/install.sh" || exit 1
+    /bin/sh "$HOME/install.sh" || exit 1
 fi
 
 # Run the startup helper script
-sh /helper.sh
+sh $HOME/helper.sh
