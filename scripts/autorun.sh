0024a332df5d3cc966c3c13a0d2ccb66c0f7b3e2
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2026-06-03 15:16:54 +0300

    Update autorun.sh
    Signed-off-by: IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -22,15 +22,6 @@ else
     echo "--- [1] System Setup: Dependencies already installed. Skipping. ---"
 fi
 
-echo "--- Enabling BBR for optimal network performance ---"
-if ! grep -q "net.core.default_qdisc=fq" /etc/sysctl.conf; then
-    echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-fi
-if ! grep -q "net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf; then
-    echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-fi
-sysctl -p
-
 # ==========================================
 # GENERATOR: xray.sh
 # ==========================================
