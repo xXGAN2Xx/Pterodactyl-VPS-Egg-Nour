930ccaf60ec212c4ec7286b96b41b48d43c9f6c3
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2026-06-10 01:26:08 +0300

    Update autorun.sh
    Signed-off-by: IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -40,7 +40,7 @@ mkdir -p "\$CONFIG_DIR"
 
 # --- Xray Core Installation ---
 echo "Checking/Installing Xray..."
-bash -c "\$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+bash -c "\$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --without-geodata
 
 # --- Port ---
 if [ -z "\${SERVER_PORT:-}" ]; then
