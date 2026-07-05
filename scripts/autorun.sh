263a5bb1e6ec6cfd4c3cea7e0832fe2c0b5d50b4
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2026-07-06 00:33:28 +0300

    Update autorun.sh
    Signed-off-by: IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -112,7 +112,7 @@ cat > "$CONFIG_PATH" << JSON
         "security": "reality",
         "realitySettings": {
           "show": false,
-          "dest": "www.microsoft.com:443",
+          "dest": "www.google.com:443",
           "xver": 0,
           "serverNames": [
             "playstation.net"
