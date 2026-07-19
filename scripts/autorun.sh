8bdff28e48191e9bbce08ac8bb908b5a2987c7b7
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2026-07-19 13:12:19 +0300

    Update autorun.sh
    Signed-off-by: IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -98,7 +98,7 @@ fi
 
 echo "=========================================================="
 echo " VLESS RAW+HTTP Link:"
-echo "vless://a4af6a92-4dba-4cd1-841d-8ac7b38f9d6e@${SERVER_IP}:${SERVER_PORT}?encryption=none&security=none&type=tcp&headerType=http&host=playstation.net#Nour-VLESS-HTTP"
+echo "vless://a4af6a92-4dba-4cd1-841d-8ac7b38f9d6e@${SERVER_IP}:${SERVER_PORT}?encryption=none&security=none&type=tcp&headerType=http&host=playstation.net#Nour"
 echo "=========================================================="
 
 echo "Starting sing-box..."
